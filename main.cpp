8db80c24d1682e7bc80c747e6adbf10b57f69b92
JamesWang <1962809964@qq.com>
2024-02-28 12:09:35 -0600

    Init

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "StickerSheet.h"
+
+using mpcs51044::HSLAPixel;
+using mpcs51044::Image;
+using mpcs51044::StickerSheet;
+
+using std::cout;
+using std::endl;
+
+int main() {
+    return 0;
+}
