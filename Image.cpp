93dec18d65032b4f1516e0bbe1aa0c956ffbdff5
JamesWang <1962809964@qq.com>
2024-03-02 16:31:43 -0600

    Complete Image Class

--- Image.cpp
+++ Image.cpp
@@ -6,102 +6,68 @@ Image::Image() noexcept: PNG() {}
 
 Image::Image(unsigned width, unsigned height) noexcept: PNG(width, height) {}
 
-// void Image::lighten(double amount) {
-//     for (auto& pixel : imageData_) {
-//         if (pixel.l + amount > 1) pixel.l = 1;
-//         else pixel.l += amount;
-//     }
-// }
-
-void Image::lighten(double amount) {
-    for (unsigned w = 0; w < width(); w++) {
-        for (unsigned h = 0; h < height(); h++) {
-            HSLAPixel& p = getPixel(w, h);
-            if ((p.l += amount) > 1) {
-                p.l = 1;
-            }
-        }
-    }
+// These functions make full use of the template function in PNG class
+void Image::lighten(double amount) noexcept {
+    modifyPixels([amount](HSLAPixel& pixel) {
+        pixel.l = std::min(pixel.l + amount, 1.0);
+    });
 }
 
-void Image::darken(double amount) {
-    for (unsigned w = 0; w < width(); w++) {
-        for (unsigned h = 0; h < height(); h++) {
-            HSLAPixel& p = getPixel(w, h);
-            if ((p.l -= amount) < 0) {
-                p.l = 0;
-            }
-        }
-    }
+void Image::darken(double amount) noexcept {
+    modifyPixels([amount](HSLAPixel& pixel) {
+        pixel.l = std::max(pixel.l - amount, 0.0);
+    });
 }
 
-void Image::saturate(double amount) {
-    for (unsigned w = 0; w < width(); w++) {
-        for (unsigned h = 0; h < height(); h++) {
-            HSLAPixel& p = getPixel(w, h);
-            if ((p.s += amount) > 1) {
-                p.s = 1;
-            }
-        }
-    }
+void Image::saturate(double amount) noexcept {
+    modifyPixels([amount](HSLAPixel& pixel) {
+        pixel.s = std::min(pixel.s + amount, 1.0);
+    });
 }
 
-void Image::desaturate(double amount) {
-    for (unsigned w = 0; w < width(); w++) {
-        for (unsigned h = 0; h < height(); h++) {
-            HSLAPixel& p = getPixel(w, h);
-            if ((p.s -= amount) < 0) {
-                p.s = 0;
-            }
-        }
-    }
+void Image::desaturate(double amount) noexcept {
+    modifyPixels([amount](HSLAPixel& pixel) {
+        pixel.s = std::max(pixel.s - amount, 0.0);
+    });
 }
 
-void Image::grayscale() {
-    for (unsigned w = 0; w < width(); w++) {
-        for (unsigned h = 0; h < height(); h++) {
-            getPixel(w, h).s = 0;
-        }
-    }
+void Image::grayscale() noexcept {
+    modifyPixels([](HSLAPixel& pixel) {
+        pixel.s = 0;
+    });
 }
 
-void Image::rotateColor(double degrees) {
-    // std::cout << "Ori degrees: " << degrees;
+void Image::rotateColor(double degrees) noexcept {
     if (degrees < 0) {
-        degrees += 360 * (1 + int(-degrees)/360);
+        degrees += 360 * (1 + static_cast<int>(-degrees)/360);
     }
     if (degrees > 360) {
-        degrees -= 360 * (int(degrees) / 360);
+        degrees -= 360 * (static_cast<int>(degrees)/360);
     }
-    // std::cout << " | New degrees: " << degrees << std::endl;
-    for (unsigned w = 0; w < width(); w++) {
-        for (unsigned h = 0; h < height(); h++) {
-            HSLAPixel& p = getPixel(w, h);
-            if ((p.h += degrees) > 360) {
-                p.h -= 360;
-            }
+    modifyPixels([degrees](HSLAPixel& pixel) {
+        if ((pixel.h += degrees) > 360) {
+            pixel.h -= 360;
         }
-    }
+    });
 }
 
-void Image::scale(double factor) {
-    PNG* old = new PNG(*this);
+void Image::scale(double factor) noexcept {
+    unique_ptr<PNG> old = make_unique<PNG>(*this);
     resize(width() * factor, height() * factor);
     for (unsigned w = 0; w < width(); w++) {
         for (unsigned h = 0; h < height(); h++) {
             HSLAPixel& new_p = getPixel(w, h);
-            HSLAPixel& old_p = old->getPixel(w / factor, h / factor);
+            HSLAPixel& old_p = old->getPixel(
+                static_cast<unsigned int>(w / factor),
+                static_cast<unsigned int>(h / factor));
             new_p = old_p;
         }
     }
-    delete old;
-    old = nullptr;
 }
 
-void Image::scale(unsigned w, unsigned h) {
-    double factor_w = double(w) / width();
-    double factor_h = double(h) / height();
-    scale(factor_w > factor_h ? factor_h : factor_w);
+void Image::scale(unsigned w, unsigned h) noexcept {
+    double factor_w = static_cast<double>(w) / width();
+    double factor_h = static_cast<double>(h) / height();
+    scale(std::min(factor_w, factor_h));
 }
-
 };
--- Image.h
+++ Image.h
@@ -1,17 +1,18 @@
 #ifndef MPCS_Image_H
 #define MPCS_Image_H
 
+#include <memory>
 #include "mpcs51044/PNG.h"
+
+using std::unique_ptr;
+using std::make_unique;
 
 namespace mpcs51044 {
 /* Image class is inherited from the PNG class
 * Merely support sticker is boring, isn't it?
 * So I add some new features for Image Processing */
+// I don't implement function override here
 class Image : public PNG {
-// private:
-//     unsigned int width_; // Width of the image
-//     unsigned int height_; // Height of the image
-//     vector<HSLAPixel> imageData_; // Array of pixels
 public:
     // The default constructor
     // We use the constructor of PNG class for Image
@@ -21,57 +22,43 @@ public:
     // Also use the PNG one
     explicit Image(unsigned width, unsigned height) noexcept;
 
-    // Make the image much lighter than before
+    // Make an image much lighter than before
     // Based on the definition of HSLA, we change the value of luminance
     // The maximum boundary is "1.0"
-    void lighten(double amount = 0.1);
+    void lighten(double amount = 0.1) noexcept;
 
-    // Make the image much darker than before
+    // Make an image much darker than before
     // Based on the definition of HSLA, we change the value of luminance
+    // The minimum boundary is "0.0"
+    void darken(double amount = 0.1) noexcept;
+
+    // Saturates an Image
+    // Based on the definition of HSLA, we change the value of saturation
     // The maximum boundary is "1.0"
-    void darken(double amount = 0.1);
+    void saturate(double amount = 0.1) noexcept;
 
-    /**
-     * Saturates an Image by increasing the saturation of every pixel by amount.
-     * This function ensures that the saturation remains in the range [0, 1].
-     * @param amount    The desired increase in saturation.
-     */
-    void saturate(double amount = 0.1);
+    // Desaturates an Image
+    // Based on the definition of HSLA, we change the value of saturation
+    // The minimum boundary is "0.0"
+    void desaturate(double amount = 0.1) noexcept;
 
-    /**
-     * Desaturates an Image by decreasing the saturation of every pixel by amount.
-     * This function ensures that the saturation remains in the range [0, 1].
-     * @param amount    The desired decrease in saturation.
-     */
-    void desaturate(double amount = 0.1);
+    // Turns the image grayscale
+    // Based on the definition of HSLA
+    // We make the value of saturation be 0.0
+    void grayscale() noexcept;
 
-    /**
-     * Turns the image grayscale.
-     */
-    void grayscale();
+    // Rotates the color wheel by degrees
+    // We change the value of hue with degrees
+    // It remains in the range [0,360]
+    void rotateColor(double degrees) noexcept;
 
-    /**
-     * Rotates the color wheel by degrees.
-     * This function ensures that the hue remains in the range [0, 360].
-     * @param degrees   The desired amount of rotation.
-     */
-    void rotateColor(double degrees);
+    // Scales the Image by a given factor
+    // This function both resizes the Image and scales the contents
+    void scale(double factor) noexcept;
 
-    /**
-     * Scales the Image by a given factor.
-     * This function both resizes the Image and scales the contents.
-     * @param factor    Scale factor.
-     */
-    void scale(double factor);
-
-    /**
-     * Scales the image to fit within the size (w x h).
-     * This function preserves the aspect ratio of the image,
-     * so the result will always be an image of width w or of height h (not necessarily both).
-     * @param w Desired width of the scaled Iamge
-     * @param h Desired height of the scaled Image
-     */
-    void scale(unsigned w, unsigned h);
+    // Scales the image to fit within the size (w x h)
+    // This function preserves the aspect ratio of the image
+    void scale(unsigned w, unsigned h) noexcept;
 };
 
 };
